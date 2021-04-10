43dea1248ca1a0a4fbed115d7d900f2c63c38f9e
Mayank Kumar <mayank963k@gmail.com>
2021-04-10 17:40:13 +0530

    BackEnd

--- app.js
+++ app.js
@@ -1,40 +1,65 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const fs = require('fs');
+const mongoose = require('mongoose');
 const app = express();
+app.use(bodyParser.json())
 const port = 80;
 
-app.use('/static', express.static('static'))       //for serving static files
+app.use('/static', express.static('static')) //for serving static files
 app.use(express.urlencoded());
 
-app.set('view engine','pug')                     // set the template engine as pug
-app.set('views', path.join(__dirname,'views'))    //set the view directory
+app.set('view engine', 'pug') // set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) //set the view directory
 
-app.get('/',(req, res)=>{
-    const params ={}
-    res.status(200).render('index.pug',params);
+mongoose.connect('mongodb://localhost/danceDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const danceSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    mobno: Number,
+    gender: String,
+
+});
+
+const Dance = mongoose.model('Dance', danceSchema);
+
+
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('index.pug', params);
 })
 
-app.get('/contact',(req, res)=>{
-    const params ={}
-    res.status(200).render('contact.pug',params);
+app.get('/contact', (req, res) => {
+    const params = {}
+    res.status(200).render('contact.pug', params);
 })
 
-app.get('/about',(req, res)=>{
-    const params ={}
-    res.status(200).render('about.pug',params);
+app.get('/about', (req, res) => {
+    const params = {}
+    res.status(200).render('about.pug', params);
 })
-app.get('/events',(req, res)=>{
-    const params ={}
-    res.status(200).render('events.pug',params);
+app.get('/events', (req, res) => {
+    const params = {}
+    res.status(200).render('events.pug', params);
 })
 
-app.post('/',(req, res)=>{
+app.post('/', (req, res,) => {
     console.log(req.body)
-    const params ={}
-    res.status(200).render('contact.pug',params)
+    const dance = new Dance(req.body.mobno)
+    dance.save()
+        .then(item => {
+            res.send("item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`The app is started on port ${port}`);
 })
